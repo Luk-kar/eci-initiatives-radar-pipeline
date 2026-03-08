abfcdb1b67bd7e7d74ef922403ca55d2a05885fb
Karol Lukaszczyk <lukkarcontact@gmail.com>
2026-03-08 14:06:04 +0100

    update

---
+++ page_to_export/script/elements/static.js
@@ -0,0 +1,4 @@
+const STATIC_PARTIALS = [
+    ["partials/header.html", "header-slot"],
+    ["partials/deep_dive_footer.html", "footer-slot"],
+];
--- page_to_export/script/partials.js
+++ page_to_export/script/partials.js
@@ -11,11 +11,7 @@ async function loadPartial(url, targetId) {
 }
 
 (async () => {
-    await loadPartial("partials/header.html", "header-slot");
-    await loadPartial("partials/deep_dive_footer.html", "footer-slot");
-    await loadPartial("partials/kpi_row.html", "kpi-slot");
-    await loadPartial("partials/chart_top_10_signatures.html", "chart1-slot");
-    await loadPartial("partials/chart_outcomes.html", "chart-initiatives-status-slot");
-    await loadPartial("partials/chart_signatures_cohorts.html", "chart-signatures-count-slot");
-    await loadPartial("partials/list_currently_open.html", "currently-open");
+    for (const [url, id] of [...STATIC_PARTIALS, ...GENERATED_PARTIALS]) {
+        await loadPartial(url, id);
+    }
 })();
