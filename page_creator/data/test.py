8e44ed5c0d856ae92a82e9c32c95a72d09145805
Karol Lukaszczyk <lukkarcontact@gmail.com>
2026-05-04 16:55:22 +0200

    init update dashboard csv columns; update example data for dashboard

--- page_creator/data/test.py
+++ page_creator/data/test.py
@@ -4,8 +4,8 @@ df = pd.read_csv("./initiatives_2026-02-09_13-24-41.csv")
 
 df = df.drop(columns=["primary_policy_area"])
 
-df.to_csv("./initiatives_2026-02-09_13-24-41.csv", index=False)
+# df.to_csv("./initiatives_2026-02-09_13-24-41.csv", index=False)
 
 # values = df["current_status"].unique()
 
-print(df.columns)
+df.to_csv("initiatives_cleaned.csv", index=False)
