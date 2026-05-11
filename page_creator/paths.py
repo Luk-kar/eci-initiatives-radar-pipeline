416a3771d49ac56fea515110230c370738c14251
Karol Lukaszczyk <lukkarcontact@gmail.com>
2026-05-11 12:50:06 +0200

    Updated `page_creator` to conform a new `page_to_export` structure.

--- page_creator/paths.py
+++ page_creator/paths.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
 PARTIALS_DIR = Path(__file__).parent / "partials"
-OUT_DIR = Path(__file__).parent.parent / "page_to_export" / "partials"
+OUT_DIR = Path(__file__).parent.parent / "page_to_export" / "generated" / "partials"
+
 GENERATED_JS = (
     Path(__file__).parent.parent
     / "page_to_export"
+    / "generated"
     / "script"
     / "elements"
     / "generated.js"
